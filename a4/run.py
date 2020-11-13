44894491db526de422c0c85be0f685dc837c6055
PatrikDurdevic <patrik.durdevic@gmail.com>
2020-11-13 13:07:45 +0100

    Solved a4

--- a4/run.py
+++ a4/run.py
@@ -286,7 +286,7 @@ def decode(args: Dict[str, str]):
         bleu_score = compute_corpus_level_bleu_score(test_data_tgt, top_hypotheses)
         print('Corpus BLEU: {}'.format(bleu_score * 100), file=sys.stderr)
 
-    with open(args['OUTPUT_FILE'], 'w') as f:
+    with open(args['OUTPUT_FILE'], 'w+') as f:
         for src_sent, hyps in zip(test_data_src, hypotheses):
             top_hyp = hyps[0]
             hyp_sent = ' '.join(top_hyp.value)
